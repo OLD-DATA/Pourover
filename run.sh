6eb4908247734658435c7a90615f69fd2670ce28
OLD-DATA <104645915+OLD-DATA@users.noreply.github.com>
2023-06-01 15:58:56 -0400

    sleep

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 echo "let there be Affogatto"
+sleep 2
 mkdir benchmark
 mkdir deps
 mkdir docs
@@ -6,6 +7,8 @@ mkdir lib
 mkdir src
 mkdir test
 mkdir tools
+echo "every floder needed"
+sleep 2
 cd deps
 git clone https://github.com/nodejs/llhttp.git
 git clone https://github.com/npm/cli.git
@@ -14,4 +17,5 @@ git clone https://github.com/madler/zlib.git
 git clone https://github.com/libuv/libuv.git
 git clone https://github.com/v8/v8.git
 ls
+sleep 2
 echo "you did it"
